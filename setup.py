6cbaee14cc97936865c41798248588c335931072
Sujay Kumar <sujaykumar47@gmail.com>
2024-10-24 23:15:52 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup # type: ignore
+
+setup(
+    name="Slowloris",
+    py_modules=["slowloris"],
+    entry_points={"console_scripts": ["slowloris=slowloris:main"]},
+    version="0.2.6",
+    description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
+    author="Sujay Kumar",
+    author_email="sujaykumar@gmail.com",
+    url="https://github.com/sujaay/slowloris",
+    keywords=["dos", "http", "slowloris"],
+    license="MIT",
+)
+
+
